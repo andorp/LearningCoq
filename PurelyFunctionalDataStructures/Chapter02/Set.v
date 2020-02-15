243681cd0ba5b062add6f7b754667f19809b9a6c
Andor Penzes <andor.penzes@gmail.com>
2020-02-15 13:44:36 +0100

    Typeclass for Set and a BST based instance.

--- PurelyFunctionalDataStructures/Chapter02/Set.v
+++ PurelyFunctionalDataStructures/Chapter02/Set.v
@@ -1,32 +1,15 @@
-
-Class set (S : Type -> Type) : Type :=
-  { empty  : forall (A : Type) , S A
-  ; insert : forall {A : Type} , A -> S A -> S A
-  ; member : forall {A : Type} , A -> S A -> bool
-
-  ; empty_member
-      : forall (A : Type) (a : A)
-      , member a (empty A) = false
-  ; insert_member
-      : forall (A : Type) (a : A) (s : S A)
-      , member a (insert a s) = true
-  ; insert_member_idenpotent
-      : forall (A : Type) (a : A) (s : S A)
-      , member a (insert a s) = member a (insert a (insert a s))
-  }.
-
 Class ordered (T : Type) :=
   { eq  : T -> T -> bool
   ; lt  : T -> T -> bool
   ; leq : T -> T -> bool
 
-  ; eq_refl
+  ; eq_sym
       : forall (t1 t2 : T)
       , eq t1 t2 = eq t2 t1
   ; lt_asym
       : forall (t1 t2 : T) (b : bool)
-      , (lt t1 t2 = b) -> (lt t2 t1) <> b
-  ; implies_leq
+      , (lt t1 t2 = true) -> (lt t2 t1 = false)
+  ; eq_implies_leq
       : forall (t1 t2 : T)
       , eq t1 t2 = true -> lt t1 t2 = true -> leq t1 t2 = true
   ; lt_not_eq
@@ -58,7 +41,7 @@ Section OrderedNat.
   Definition nat_leq (n0 n1 : nat) : bool :=
     nat_eq n0 n1 || nat_lt n0 n1.
     
-  Theorem nat_eq_refl
+  Theorem nat_eq_sym
     : forall (t1 t2 : nat)
     , nat_eq t1 t2 = nat_eq t2 t1.
   Proof.
@@ -66,11 +49,11 @@ Section OrderedNat.
 
   Theorem nat_lt_asym
     : forall (t1 t2 : nat) (b : bool)
-    , (nat_lt t1 t2 = b) -> (nat_lt t2 t1) <> b.
+    , (nat_lt t1 t2 = true) -> (nat_lt t2 t1 = false).
   Proof.
   Admitted.
 
-  Theorem nat_implies_leq
+  Theorem nat_eq_implies_leq
     : forall (t1 t2 : nat)
     , nat_eq t1 t2 = true -> nat_lt t1 t2 = true -> nat_leq t1 t2 = true.
   Proof.
@@ -82,11 +65,36 @@ Section OrderedNat.
   Proof.
   Admitted.
 
+  Instance orderedNat : ordered nat :=
+    { eq              := nat_eq
+    ; lt              := nat_lt
+    ; leq             := nat_leq
+    ; eq_sym          := nat_eq_sym
+    ; lt_asym         := nat_lt_asym
+    ; eq_implies_leq  := nat_eq_implies_leq
+    ; lt_not_eq       := nat_lt_not_eq
+    }.
+
 End OrderedNat.
 
+Class set (S : Type) (A : Type) : Type :=
+  { empty  : S
+  ; insert : A -> S -> S
+  ; member : A -> S -> bool
+
+  ; empty_member
+      : forall (a : A)
+      , member a empty = false
+  ; insert_non_member
+      : forall (s : S) (a : A)
+      , member a s = false -> member a (insert a s) = true
+  ; insert_member_idenpotent
+      : forall (s : S) (a : A)
+      , member a (insert a s) = member a (insert a (insert a s))
+  }.
 
 
-Section BinaryTree.
+Section BinarySearchTree.
 
   Inductive tree (A : Type) : Type :=
     | Leaf   : tree A
@@ -123,49 +131,42 @@ Section BinaryTree.
           end
     end.
 
-End BinaryTree.
-
-Print tree_member.
+End BinarySearchTree.
 
 Section UnbalancedSet.
 
-(* Theorem tree_empty_member
-  : forall (A : Type) (a : A)
+Theorem tree_empty_member
+  : forall (A : Type) (o : ordered A) (a : A)
   , tree_member a (Leaf A) = false.
 Proof.
-Admitted. *)
+Admitted.
 
 Theorem tree_insert_member
-  : forall (A : Type) `{o : ordered A} (a : A) (s : tree A)
+  : forall (A : Type) (o : ordered A) (s : tree A) (a : A)
   , tree_member a (tree_insert a s) = true.
 Proof.
 Admitted.
 
+Theorem tree_insert_non_member
+  : forall (A : Type) (o : ordered A) (s : tree A) (a : A)
+  , tree_member a s = false -> tree_member a (tree_insert a s) = true.
+Proof.
+Admitted.
+
 Theorem tree_insert_member_idenpotent
-  : forall (A : Type) `{o : ordered A} (a : A) (s : tree A)
+  : forall (A : Type) (o : ordered A) (s : tree A) (a : A)
   , tree_member a (tree_insert a s) = tree_member a (tree_insert a (tree_insert a s)).
 Proof.
 Admitted.
 
-Instance unbalanced_set_tree : set tree :=
-  { empty  := Leaf
+Instance unbalanced_set_tree {A : Type} `{o : ordered A} : set (tree A) A :=
+  { empty                     := Leaf A
+  ; insert                    := tree_insert
+  ; member                    := tree_member
+  ; empty_member              := tree_empty_member _ _
+  ; insert_non_member         := tree_insert_non_member _ _
+  ; insert_member_idenpotent  := tree_insert_member_idenpotent _ _
   }.
-Abort.
-(* 
-Class set (S : Type -> Type) : Type :=
-  { empty  : forall (A : Type) , S A
-  ; insert : forall {A : Type} , A -> S A -> S A
-  ; member : forall {A : Type} , A -> S A -> bool
-
-  ; empty_member
-      : forall (A : Type) (a : A)
-      , member a (empty A) = false
-  ; insert_member
-      : forall (A : Type) (a : A) (s : S A)
-      , member a (insert a s) = true
-  ; insert_member_idenpotent
-      : forall (A : Type) (a : A) (s : S A)
-      , member a (insert a s) = member a (insert a (insert a s))
-  }. *)
 
 End UnbalancedSet.
+
